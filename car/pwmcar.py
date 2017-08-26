a399bdb4ddaca9c843b114aca014d069d76717da
Philip Zucker <you@example.com>
2017-08-26 19:23:49 +0000

    added sensor to pwmcar

--- car/pwmcar.py
+++ car/pwmcar.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 
 class PWMCar():
-	def __init__(self, fwd = 16, rev = 18, left = 26, right = 19 ):
+	def __init__(self, fwd = 16, rev = 18, left = 26, right = 19, irSensor=17 ):
 
 		GPIO.setmode(GPIO.BCM)
-
+		
 		GPIO.setup(fwd,GPIO.OUT)
 		GPIO.setup(rev,GPIO.OUT)
 		GPIO.setup(left,GPIO.OUT)
 		GPIO.setup(right,GPIO.OUT)
+		GPIO.setup(irSensor, GPIO.IN)
 
+		self.irSensorPin = irSensor
 		self.forwardPWM = GPIO.PWM(fwd, 100)
 		self.reversePWM = GPIO.PWM(rev, 100)
 		self.leftPWM = GPIO.PWM(left, 100)
@@ -42,3 +44,6 @@ class PWMCar():
 	def stop(self):
 		self.forwardPWM.stop()
 		self.reversePWM.stop()
+
+	def isHome(self):
+		return not GPIO.input(self.irSensorPin) #gpio returns 1 if no ir, 0 if sense ir
