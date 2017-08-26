e76dbcb20d4f6feb51cca34ca9ec21a7bb25a850
Ben <ishmandoo@gmail.com>
2017-08-26 17:43:08 -0400

    added random test

---
+++ test/random_move.py
@@ -0,0 +1,29 @@
+from car import pwmcar
+import time
+from random import randint
+
+car = pwmcar.PWMCar()
+
+car.forward()
+
+while not car.isHome():
+	print "Not Home"
+	moveRandom()
+	time.sleep(1)
+
+def moveRandom():
+	steer = randint(0,2)
+	dir = randint(0,1)
+	if steer == 0:
+		car.center()
+	elif steer == 1:
+		car.right()
+	elif steer == 2:
+		car.left()
+	if dir == 0:
+		car.forward()
+	elif dir == 1:
+		car.reverse()
+
+
+car.stop()
